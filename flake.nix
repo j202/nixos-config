7b56115065315780a59a1a7b0f11737d4e22404d
j202 <j202@users.noreply.github.com>
2026-05-02 12:30:17 +0100

    use stable home manager for stable config

--- flake.nix
+++ flake.nix
@@ -13,10 +13,15 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
     catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs-stable, nixpkgs-unstable, home-manager, catppuccin, ... }: {
+  outputs = { self, nixpkgs-stable, nixpkgs-unstable, home-manager, home-manager-stable, catppuccin, ... }: {
     nixosConfigurations = {
 
       # Dell XPS M1330 — pinned to nixos-25.11 stable
@@ -24,7 +29,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/xpsm1330/configuration.nix
-          home-manager.nixosModules.home-manager
+          home-manager-stable.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
